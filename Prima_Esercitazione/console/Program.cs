0457df72f93fac010fbbfa2d81929805447a3033
unknown <ivaldi500@gmail.com>
2024-06-19 15:50:03 +0200

    Metodi di Console

--- Prima_Esercitazione/console/Program.cs
+++ Prima_Esercitazione/console/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Write("il tuo nome: ");
+Console.WriteLine("Sergej");
+Console.WriteLine("weeee \n");
+Console.Write("tutto beneee? \t wewewewewe ");
+Console.WriteLine("Scrivimi il tuo nome!");
+/*
+ConsoleKeyInfo cki;
+do{
+    cki = Console.ReadKey();
+    Console.Write("BLA BLA BLA ORA \n Premi esc per uscire \n");
+} while(cki.Key != ConsoleKey.Escape);
+*/
+string nome2 = "Luigi";
+string nome = Console.ReadLine()!;
+Console.WriteLine("ciao " + nome + " è un piacere conoscerti!");
+
+Console.WriteLine("Scrivimi il tuo cognome!");
+string cognome = Console.ReadLine()!;
+Console.WriteLine($"ahhh, {cognome}! che bel cognome");
+Console.WriteLine($"quindni ti chiami {nome} {nome2} {cognome}.");
