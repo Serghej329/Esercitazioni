48caff3c03804916119e4c92ce76066a0a5cef2f
unknown <ivaldi500@gmail.com>
2024-06-21 15:09:10 +0200

    Esercizio sugli Switch

---
+++ 06-Assignment/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("|scegli la data| \n 1)Lunedì \n 2)Martedì \n 3)Mercoledì \n 4)Giovedì \n 5)Venerdì \n 6)Sabato \n 7)Domenica \n Scrivimi il numero");
+int giorno = Convert.ToInt32(Console.ReadLine());
+
+switch (giorno)
+{
+    case 1:
+        Console.WriteLine("Lunedì");
+        break;
+
+    case 2:
+        Console.WriteLine("Martedì");
+        break;
+
+    case 3:
+        Console.WriteLine("Mercoledi");
+        break;
+
+    case 4:
+        Console.WriteLine("Giovedì");
+        break;
+
+    case 5:
+        Console.WriteLine("Venerdì");
+        break;
+
+    case 6:
+        Console.WriteLine("Sabato");
+        break;
+
+    case 7:
+        Console.WriteLine("Domenica");
+        break;
+
+    default:
+        Console.WriteLine("Il numero non corrisponde a nessungiorno della settimana");
+        break;
+}
---
+++ 06-Assignment/README.md
@@ -0,0 +1,3 @@
+# 06-ASSIGNMENT 2
+
+### Creare una console app che chieda all'utente di inseirire l'identificativo numerico riferito ad un giorno della settimana (numeri da 1 a 7) e che restituisca all'tente il giorno in stringa
