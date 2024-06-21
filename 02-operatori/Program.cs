fa13d42a370a2361295ddc7f2ae420980ff50d00
unknown <ivaldi500@gmail.com>
2024-06-21 11:15:24 +0200

    Tipi di operatori

---
+++ 02-operatori/Program.cs
@@ -0,0 +1,55 @@
+﻿//Operatori aritmetici
+int a = 10;
+
+int b = 20;
+
+int somma = a+b;
+
+int sottrazione = a-b;
+
+int divisione = a/b;
+
+int modulo = a%b;
+
+int prodotto = a*b;
+
+
+//Operatori logici 
+int c = 30;
+
+int c == a;             // false
+
+int c != a;             // true
+
+int c < a;             // true
+
+int c > a;              // false
+
+int c <= a;             // true
+
+int c >= a;             // false
+
+
+//Operatori di considizionamento
+bool x = true
+
+bool y = false
+//and
+bool z = x && y;
+//or
+bool z = x || y;
+//not
+bool v = !x;
+
+
+// Esempio di utilizzo di operatori di incremento e decremento
+int d = 10;
+
+ d++;
+ d-- ;
+
+// Esempio di utilizzo di operatori di incremento e decremento
+int e=10
+
+e +=5;
+e -=5;
