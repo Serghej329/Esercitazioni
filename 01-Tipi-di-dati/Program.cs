848fc60c28f1383dbfd7a06c36b92b9dc76ee0bb
unknown <ivaldi500@gmail.com>
2024-06-21 10:44:36 +0200

    Tipi di dati/variabili ecc...

---
+++ 01-Tipi-di-dati/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int num;  //dichiaro una variabile di tipo intero.
+
+string name; //dichiaro una variabile di tipo string.
+
+bool flag; //dichiaro una variabile di tipo booleano.
+
+num =1;
+
+name = "serghej";
+
+flag = false;
+
+const double PI = 3.14;
+ Console.WriteLine(PI);
+
+ var altezza = 1.80;
+ Console.WriteLine($"qecco i tuoi valori! \n {num} {name} {flag} {PI} {altezza}.");
