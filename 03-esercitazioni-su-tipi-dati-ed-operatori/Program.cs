c139e38dfaa69821c91c815822778166cf316324
unknown <ivaldi500@gmail.com>
2024-06-21 12:06:22 +0200

    esercizio per tipi di dati e operatori

---
+++ 03-esercitazioni-su-tipi-dati-ed-operatori/Program.cs
@@ -0,0 +1,31 @@
+﻿//pulire il terminale 
+Console.Clear();
+
+//stampa il valore di una variabile
+int num = 10;
+
+Console.WriteLine($"ecco i tuoi valore | \n                   V\n                   {num}");
+
+Console.WriteLine("");
+
+int eta = 20;
+eta ++;
+Console.WriteLine($"ecco la tua eta    | \n                   V\n                   {eta}");
+
+Console.WriteLine("");
+
+int eta2 = 20;
+eta2 --;
+Console.WriteLine($"ecco la tua eta    | \n                   V\n                   {eta2}");
+
+Console.WriteLine("");
+
+int eta3 = 20;
+eta3 += 10;
+Console.WriteLine($"ecco la tua eta    | \n                   V\n                   {eta3}");
+
+Console.WriteLine("");
+
+int eta4 = 20;
+eta4 -= 10;
+Console.WriteLine($"ecco la tua eta    | \n                   V\n                   {eta4}");
--- 03-esercitazioni-su-tipi-dati-ed-operatori/obj/Debug/net8.0/03-esercitazioni-su-tipi-dati-ed-operatori.AssemblyInfo.cs
+++ 03-esercitazioni-su-tipi-dati-ed-operatori/obj/Debug/net8.0/03-esercitazioni-su-tipi-dati-ed-operatori.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("console")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("03-esercitazioni-su-tipi-dati-ed-operatori")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa13d42a370a2361295ddc7f2ae420980ff50d00")]
-[assembly: System.Reflection.AssemblyProductAttribute("console")]
-[assembly: System.Reflection.AssemblyTitleAttribute("console")]
+[assembly: System.Reflection.AssemblyProductAttribute("03-esercitazioni-su-tipi-dati-ed-operatori")]
+[assembly: System.Reflection.AssemblyTitleAttribute("03-esercitazioni-su-tipi-dati-ed-operatori")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generato dalla classe WriteCodeFragment di MSBuild.
