e60064a4d0705c68f4462d0452dea6a5b69b488a
Serghej329 <ivaldi500@gmail.com>
2024-06-28 16:00:22 +0200

    Esercizio-Elenco con Menu interattivoàù

---
+++ 22-Menu-Lista/Program.cs
@@ -0,0 +1,33 @@
+﻿
+List<string> nomi = new List<string>();
+bool t = true;
+while (t)
+{
+    //MENU'
+    Console.WriteLine("\t|MENU' LISTA| \n\n    Fai la tua scelta' \n 1)visualizza lista \n 2)Aggiungi partecipante \n 3)Esci \n ");
+    int scelta = int.Parse(Console.ReadLine()!);
+
+    switch (scelta)
+    {
+        case 1: // Visualizzazione Partecipanti in lista
+            Console.WriteLine($"\n|Lista|");
+            foreach (string nomeLista in nomi) Console.WriteLine(nomeLista);
+            break;
+        case 2: // Aggiunta Partecipanti in lista
+            Console.WriteLine("Scrivi il nome del partecipante: \n");
+            string partecipante = Console.ReadLine()!;
+            nomi.Add(partecipante);
+            break;
+
+        case 3: // TODO DA SISTERMARE
+            Console.ReadKey();
+            t = false;
+            break;
+
+        default:// Se l'operazione non è valida
+            Console.WriteLine("Il numero non corrisponde a nessun delle operazioni "); // Stampa un messaggio di errore
+            break;
+
+    }
+}
+
