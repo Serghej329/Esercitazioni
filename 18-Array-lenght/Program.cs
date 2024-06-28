8bbe7c0eaf38ff7e18b8ba6a5f9b1c5f979094c5
Serghej329 <ivaldi500@gmail.com>
2024-06-28 10:38:20 +0200

    Lunghezza Array

---
+++ 18-Array-lenght/Program.cs
@@ -0,0 +1,8 @@
+﻿
+int[] num = new int[4];
+num[0] = 5;
+num[1] = 10;
+num[2] = 20;
+num[3] = 20;
+foreach (int s in num) Console.WriteLine(s);
+Console.WriteLine($"il numero di elementi è {num.Length}");
