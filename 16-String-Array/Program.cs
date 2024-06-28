2bac38839874a0e6069be1233c7a97c275d7dde5
Serghej329 <ivaldi500@gmail.com>
2024-06-28 10:29:53 +0200

    int Array

--- 16-String-Array/Program.cs
+++ 16-String-Array/Program.cs
@@ -1,6 +1,6 @@
 ﻿int Max = 3;
 string [] nomi = new string[Max];
 nomi[0] = "Mario";
-nomi[1] = "Luigi";
+nomi[1] = 5;
 nomi[2] = "Giovanni";
 foreach (string s in nomi) Console.WriteLine(s);
---
+++ 17-int-array/Program.cs
@@ -0,0 +1,7 @@
+﻿int Max = 4;
+int [] num = new int[Max];
+num[0] = 5;
+num[1] = 10;
+num[2] = 20;
+num[3] = 20;
+foreach (int s in num) Console.WriteLine(s);
