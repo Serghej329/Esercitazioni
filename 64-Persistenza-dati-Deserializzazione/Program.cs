120c497a38b8d183688a0ef55e268b68639b84c6
Serghej329 <ivaldi500@gmail.com>
2024-07-17 12:45:05 +0200

    Serializzazione e Deserializzazione file Json

---
+++ 64-Persistenza-dati-Deserializzazione/Program.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string path = @"test.json";
+        string json = File.ReadAllText(path);
+        dynamic obj = JsonConvert.DeserializeObject(json)!;
+        Console.WriteLine($"nome:{obj.nome}\n Cognome: {obj.cognome}\n Eta: {obj.eta}");
+        Console.WriteLine($"Via:{obj.indirizzo.via}\n Citta: {obj.indirizzo.citta}");
+    }
+}
+
