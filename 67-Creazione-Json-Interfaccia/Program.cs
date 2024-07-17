7dda5b2cb6ad0d816f24a36860c09179f836c0c2
Serghej329 <ivaldi500@gmail.com>
2024-07-17 15:35:44 +0200

    lettura e scrittura da Json a csv e viceversa

---
+++ 67-Creazione-Json-Interfaccia/Program.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string path = @"test.Json";
+
+        File.Create(path).Close();
+        File.AppendAllText(path, "[\n");
+
+        while (true)
+        {
+            Console.WriteLine("Scrivi il nome e prezzo:");
+            string nome = Console.ReadLine()!;
+            string prezzo = Console.ReadLine()!;
+
+            string jsonString = JsonConvert.SerializeObject(new { nome, prezzo }, Formatting.Indented);
+            File.AppendAllText(path,jsonString + ",\n");
+
+            Console.WriteLine("Vuoi inserire un'altro prodotto? (s/n)");
+            string risposta = Console.ReadLine()!;
+            if (risposta == "n")
+            {
+                break;
+            }
+        }
+        string file = File.ReadAllText(path);
+        file = file.Remove(file.Length - 2, 1);
+        File.WriteAllText(path, file);
+        File.AppendAllText(path, "]");
+    }
+}
+
